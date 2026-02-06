b16cdcc5f67250a29e1381b96066fcf69c525dfb
Hyun731 <gilhyun.gwon0731@gmail.com>
2026-02-06 17:48:33 +0900

    fix: 부모 생성자 누락 수정

--- app/repositories/concentration_repository.py
+++ app/repositories/concentration_repository.py
@@ -2,6 +2,7 @@ from app.repositories.base_repository import BaseRepository
 from app.models import Concentration
 
 class ConcentrationRepository(BaseRepository[Concentration]):
-    pass
+    def __init__(self):
+        super().__init__(Concentration)
 
 concentration_repo = ConcentrationRepository()
